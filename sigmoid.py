ed520b4570df2ec450777a52246383441321cef0
Drew Meyer <drewmeyer28@gmail.com>
2023-07-15 23:26:55 -0500

    Create sigmoid.py

---
+++ sigmoid.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sigmoid(z):
+    """
+    Computes the sigmoid of function z
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+    """
+    g = 1 / (1 + np.exp(-z))
+    return g
