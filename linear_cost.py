9fe8dda404d90f4e3ad46251442a87353924537d
Drew Meyer <drewmeyer28@gmail.com>
2023-07-15 23:53:08 -0500

    Create linear_cost.py

---
+++ linear_cost.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def compute_cost(x, y, w, b):
+  """
+  Computes the cost function for linear regression.
+
+  Args:
+    x (ndarray (m,)): Data, m examples
+    y (ndarray (m,)): target values
+    w,b (scalar)    : model parameters
+
+  Returns:
+    total_cost (float): The cost of using w,b as the parameters for linear regression to fit the data points in x and y
+  """
+  m = x.shape[0]   # get the number of training examples
+
+  cost_sum = 0
+  for i in range(m):   # for each training example
+    f_wb = w * x[i] + b   # model for single variable linear regression
+    cost = (f_wb - y[i]) ** 2   # compute mean squared error for ith term
+    cost_sum += cost   # compute sum of each term's mean squared error
+  total_cost = (1 / (2 * m)) * cost_sum   #  divide sum by 1/2m
+
+  return total_cost   # return total cost
