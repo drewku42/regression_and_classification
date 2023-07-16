a1316617c0dfc3e83925c2e608c82d011945f160
Drew Meyer <drewmeyer28@gmail.com>
2023-07-15 23:44:11 -0500

    Update sigmoid.py

--- sigmoid.py
+++ sigmoid.py
@@ -5,8 +5,11 @@ def sigmoid(z):
     Computes the sigmoid of function z
     Args:
         z (ndarray): A scalar, numpy array of any size.
+        
     Returns:
         g (ndarray): sigmoid(z), with the same shape as z
     """
-    g = 1 / (1 + np.exp(-z))
+    
+    g = 1 / (1 + np.exp(-z)) #python representation of the sigmoid function
+    
     return g
